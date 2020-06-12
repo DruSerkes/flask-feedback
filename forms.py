d6f9a27deea4b5a04588e28639c11a50d25e7593
Dru Serkes <DruSerkes@gmail.com>
2020-06-12 16:41:32 -0400

    setup scripts

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired
+
+
+class RegisterForm(FlaskForm):
+    """ Registration form model """
+    
